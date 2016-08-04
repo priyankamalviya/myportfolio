c434d942f636a285ed4ce4d583c08ce8faceb053
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-08-03 20:33:48 -0700

    fixed hamburger menu animation

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,25 +1,50 @@
-//Youtube Section
-$(window).scroll(function() {
-    youtubeVidScroll();
-});
-
-function youtubeVidScroll() {
-
-    var wScroll = $(window).scrollTop();
-
-    //console.log(wScroll);
-    $('.video-strip').css('background-position','center -'+ wScroll +'px');
-}
-
-//tada effect
 $(function() {
     setInterval(function(){articleTada()}, 4000);
+    smoothScroll(300);
+    mobileNav();
 });
+
+
+function mobileNav() {
+    $('.mobile-nav-toggle').on('click', function(){
+        var status = $(this).hasClass('is-open');
+        if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+        else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+    });
+}
+
+
+function smoothScroll (duration) {
+    $('a[href^="#"]').on('click', function(event) {
+
+        var target = $( $(this).attr('href') );
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, duration);
+        }
+    });
+}
 
 function articleTada(){
     var randNum = Math.floor(Math.random() * $('.article-thumb').length) +1
     $('.article-thumb').eq(randNum).addClass('is-emph')
         .siblings().removeClass('is-emph');
+}
+
+$(window).scroll(function() {
+    youtubeVidScroll();
+    startArticles();
+});
+
+
+function youtubeVidScroll() {
+
+    var wScroll = $(window).scrollTop();
+
+    $('.video-strip').css('background-position','center -'+ wScroll +'px');
 }
 
 function startArticles(){
@@ -29,11 +54,7 @@ function startArticles(){
         $('.article-thumb').each(function(i){
             setTimeout(function(){
                 $('.article-thumb').eq(i).addClass('is-visible');
-            }, 200 * i);
+            }, 100 * i);
         });
     }
 }
-
-$(window).scroll(function(){
-    startArticles();
-});
